a8131b387be5038c65880216969ebc7da006ea68
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2022-06-22 19:29:22 -0400

    Completed more of ScrumDinger

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,46 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Jordan Haynes on 6/22/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(5)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(5)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
