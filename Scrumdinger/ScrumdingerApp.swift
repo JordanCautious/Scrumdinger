888aae4ec70587da330690c7d1c8e585614886b4
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2022-06-21 22:43:12 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jordan Haynes on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
